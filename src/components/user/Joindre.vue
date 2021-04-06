b7789756fa1dac002938910b9f13007c76638fe4
Dabrecks <guillaume_labrecque@outlook.com>
2021-04-06 17:19:05 -0400

    ajout joindre

--- src/components/user/Joindre.vue
+++ src/components/user/Joindre.vue
@@ -1,7 +1,47 @@
 <template>
   <div>
       <Header titreHeader="Nous joindre"/>
-      <h1>Nous joindre</h1>
+      <b-container fluid class="corp">
+        <b-row class="titre">
+          <h1 class="mb-5">Vous avez des questions?</h1>
+        </b-row>
+        <b-row class="formulaireCourriel text-left ml-1">
+          <b-form>
+            <b-form-group
+              id="input-group-1"
+              label="Email address:"
+              label-for="input-1"
+              description="We'll never share your email with anyone else."
+            >
+              <b-form-input
+                id="input-1"
+                type="email"
+                placeholder="Enter email"
+                required
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+              <b-form-input
+                id="input-2"
+                placeholder="Enter name"
+                required
+              ></b-form-input>
+            </b-form-group>
+
+            <div>
+              <b-form-textarea
+                id="textarea-rows"
+                placeholder="Tall textarea"
+                rows="8"
+              ></b-form-textarea>
+            </div>
+
+
+            <b-button class="mt-3" type="submit" variant="primary">Submit</b-button>
+          </b-form>
+        </b-row>
+      </b-container>  
       <Footer />
   </div>
 </template>
